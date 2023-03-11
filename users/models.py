383dcba9a078af3a649f144b2ddb96ee4e3885df
Phawit Pornwattanakul <me@helloyeew.dev>
2023-03-12 01:56:43 +0700

    ✨ Initialize Settings model and page for user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+THEME_SETTINGS = (
+    ('', 'Default'),
+    ('green', 'Green')
+)
+
+
+class Settings(models.Model):
+    """Settings model."""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    theme = models.CharField(max_length=20, choices=THEME_SETTINGS, default='', blank=True)
+
+    class Meta:
+        """Meta class."""
+
+        verbose_name = 'Setting'
+        verbose_name_plural = 'Settings'
+
+    def __str__(self):
+        """Return name."""
+        return self.user.username + ' settings'
--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import views as auth_views
+
+from users.forms import UserSettingsForm
 
 
 class LogoutAndRedirect(auth_views.LogoutView):
@@ -23,3 +26,18 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'users/signup.html', {'form': form})
+
+
+@login_required
+def settings(request):
+    if request.method == 'POST':
+        form = UserSettingsForm(request.POST, instance=request.user.settings)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Settings saved successfully.')
+            return redirect('settings')
+    else:
+        form = UserSettingsForm(instance=request.user.settings)
+    return render(request, 'users/settings.html', {
+        'form': form
+    })
